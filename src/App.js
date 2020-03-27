bc65c0a91b8656169bcc73982b9c108e8d66e601
Christian <christiancorby@gmail.com>
2020-03-27 13:57:25 -0400

    Lunch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
-
+import SelectBox from "./components/pizzaSize"
+import Toppings from "./components/pizzaToppings"
+import ToggleSwitch from "./components/pizzaSub";
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <h2>Build Your Own Pizza</h2>
+      <img src = "https://image.shutterstock.com/image-photo/pizza-ham-salami-mushrooms-ingredients-260nw-661996804.jpg" ></img>
+      <h3>Build Your Own Pizza</h3>
+      <h4>Choice of Size</h4>
+      <p>Required</p>
+      <SelectBox
+        items ={[
+          {value: 'Small', id:1},
+          {value: 'Medium', id:2},
+          {value: 'Large', id:3},
+        ]}
+      />
+      <h4>Choice of Sauce</h4>
+      <p>Required</p>
+      <SelectBox
+      items ={[
+        {value: 'Original Red', id:1},
+        {value: 'Garlic Ranch', id:2},
+        {value: 'BBQ Sauce', id:3},
+        {value: 'Spinach Alfredo', id:4},]}
+        />
+      <h4>Add Toppings</h4>
+      <p>Choose Up to 10</p>
+      <Toppings/>
+      <h4>Choice of Substitute</h4>
+      <p>Gluten Free Crust?</p>
+      <ToggleSwitch/>
+      <h4>Special Instructions</h4>
+      <pizzaSpecial/>
     </>
   );
 };
---
+++ src/components/pizzaToppings.js
@@ -0,0 +1,28 @@
+import React from "react";
+class Toppings extends React.Component{
+render(){
+    return <div>
+        <input type = "Checkbox"></input><p>Pepperoni</p>
+        <input type = "Checkbox"></input><p>Sausage</p>
+        <input type = "Checkbox"></input><p>Canadian Bacon</p>
+        <input type = "Checkbox"></input> <p>Spicy Italian Sausage</p>
+        <input type = "Checkbox"></input> <p>Grilled Chicken</p>
+        <input type = "Checkbox"></input>  <p>Onions</p>
+        <input type = "Checkbox"></input> <p>Green Peppers</p>
+        <input type = "Checkbox"></input> <p>Diced Tomato</p>
+        <input type = "Checkbox"></input> <p>Black Olives</p>
+        <input type = "Checkbox"></input><p>Roasted Garlic</p>
+        <input type = "Checkbox"></input>  <p>Artichoke Hearts</p>
+        <input type = "Checkbox"></input><p>Three Cheese</p>
+        <input type = "Checkbox"></input> <p>Pineapple</p>
+        <input type = "Checkbox"></input>  <p>Even More Cheese</p>
+        <input type = "Checkbox"></input> <p>Is anybody going to read these?</p>
+    
+    
+    
+    
+    
+    </div>
+}
+}
+export default Toppings;
